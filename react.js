119674854a8d34407207154ae3b8022fac61ea63
saifi61 <saifiqasim61@gmail.com>
2024-08-09 21:25:08 +0530

    added Light And Dark Mode

--- react.js
+++ react.js
@@ -1,37 +1,50 @@
 let root = document.querySelector("#root");
 
-function Header() {
-    return (
-        <nav>
-            <img src="./icon/logo_dark.svg" width="60px"></img>
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
-        </nav>
-    )
+function Header(props) {
+  return (
+    <nav className={props.darkMode ? "dark": ""}>
+      <img 
+        className="nav--logo_icon"
+        src="./icon/logo_dark.svg" width="60px"
+    />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={props.toggleDarkMode}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
+    </nav>
+  );
 }
 
-function Main() {
-    return (
-        <main>
-            <h1>Fun Facts About React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Meta</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-        </main>
-    )
+function Main(props) {
+  return (
+    <main className={props.darkMode ? "dark" : ""}>
+      <h1 className="main--title">Fun Facts About React</h1>
+      <ul className="main--facts">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
 }
 
 function App() {
-    return (
-        <div>
-            <Header />  
-            <Main />
-        </div>
-    )
+    const [darkMode, setDarkMode] = React.useState(true);
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
+  return (
+    <div className="container">
+      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+      <Main darkMode={darkMode} />
+    </div>
+  );
 }
 
-ReactDOM.render(<App />,root);
+ReactDOM.render(<App />, root);
